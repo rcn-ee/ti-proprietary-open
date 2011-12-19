003e77b86b1e90572594f64ff8db9aecda288cbf
Vishal Mahaveer <vishalm@ti.com>
2011-12-19 17:46:19 -0600

    proprietary-open - Update wl12xx/wlan firmware
    Update WLAN firmware to R4_SP2 version
    
    Change-Id: Ibb4eb34f113e93c4c21496d0a0f592b2d66c26b8
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- wl12xx/wlan/wl12xx-wlan-fw-products.mk
+++ wl12xx/wlan/wl12xx-wlan-fw-products.mk
@@ -1,7 +1,7 @@
 PRODUCT_COPY_FILES += \
-        device/ti/proprietary-open/wl12xx/wlan/wl128x-fw-r4sp1-multirole-roc.bin:system/etc/firmware/ti-connectivity/wl128x-fw-multirole-roc.bin \
-        device/ti/proprietary-open/wl12xx/wlan/wl128x-fw-r4sp1-multirole-plt.bin:system/etc/firmware/ti-connectivity/wl128x-fw-multirole-plt.bin \
+        device/ti/proprietary-open/wl12xx/wlan/wl128x-fw-r4sp2-multirole-roc.bin:system/etc/firmware/ti-connectivity/wl128x-fw-multirole-roc.bin \
+        device/ti/proprietary-open/wl12xx/wlan/wl128x-fw-r4sp2-multirole-plt.bin:system/etc/firmware/ti-connectivity/wl128x-fw-multirole-plt.bin \
         device/ti/proprietary-open/wl12xx/wlan/wl1271-nvs.bin:system/etc/firmware/ti-connectivity/wl1271-nvs.bin \
-        device/ti/proprietary-open/wl12xx/wlan/wl1271-fw-r4sp1-multirole-roc.bin:system/etc/firmware/ti-connectivity/wl1271-fw-multirole-roc.bin \
-        device/ti/proprietary-open/wl12xx/wlan/wl1271-fw-r4sp1-multirole-plt.bin:system/etc/firmware/ti-connectivity/wl1271-fw-multirole-plt.bin \
+        device/ti/proprietary-open/wl12xx/wlan/wl1271-fw-r4sp2-multirole-roc.bin:system/etc/firmware/ti-connectivity/wl1271-fw-multirole-roc.bin \
+        device/ti/proprietary-open/wl12xx/wlan/wl1271-fw-r4sp2-multirole-plt.bin:system/etc/firmware/ti-connectivity/wl1271-fw-multirole-plt.bin \
         device/ti/proprietary-open/wl12xx/wlan/wl1271-nvs_127x.bin:system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin
