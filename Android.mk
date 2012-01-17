091f3c822c82d24bddc3cad2efc6a805c22559bd
James W. Mills <jameswmills@ti.com>
2012-01-17 14:25:14 -0600

    Ducati binary fw and DCC files for Blaze Tablet
    This patch includes the files needed to install Ducati fw and DCC
    files for the Blaze Tablet.
    
    Versions of Ducati can be dropped in with an accompanying mk file, and
    each device capable of using that version of ducati can include the
    makefile at the device level
    
    Change-Id: I1df3fb99eac48106032a404184f09668d0148cda
    Signed-off-by: James W. Mills <jameswmills@ti.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,3 @@
 LOCAL_PATH := $(my-dir)
 $(hide $(shell $(LOCAL_PATH)/untar.sh $(LOCAL_PATH)/omap4 sgx $(LOCAL_PATH)/omap4/sgx.tgz))
-$(hide $(shell $(LOCAL_PATH)/untar.sh $(LOCAL_PATH)/omap4 ducati $(LOCAL_PATH)/omap4/ducati.tgz))
 include $(call all-subdir-makefiles)
