48e61cacb1c933712b8e607d9d329e86db50d5e4
Pavan Savoy <pavan_savoy@ti.com>
2012-02-02 12:06:57 -0600

    device:ti:proprietary-open: add wpan firmware
    Add the wpan.tgz firmware tarball which constitutes of the firmware and the
    right LICENSE for them.
    Also add in the line in Android.mk to untar them to the relevant folder from
    where they will be picked up to be installed into the relevant folder on
    /system/etc/firmware/.
    
    Change-Id: Ie2a7af0febb072862bbeb45039625ba810923766
    Signed-off-by: Pavan Savoy <pavan_savoy@ti.com>
    
    Conflicts:
    
    	Android.mk
    
    Signed-off-by: Pavan Savoy <pavan_savoy@ti.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
 LOCAL_PATH := $(my-dir)
 $(hide $(shell $(LOCAL_PATH)/untar.sh $(LOCAL_PATH)/omap4 sgx $(LOCAL_PATH)/omap4/sgx.tgz))
+$(hide $(shell $(LOCAL_PATH)/untar.sh $(LOCAL_PATH)/wl12xx wpan $(LOCAL_PATH)/wl12xx/wpan.tgz))
 include $(call all-subdir-makefiles)
