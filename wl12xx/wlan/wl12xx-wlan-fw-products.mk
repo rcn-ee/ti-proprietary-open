4f8202cd832f1a8688a17c83a7e4eb1f66ea6cc8
Panduranga Mallireddy <panduranga_mallireddy@ti.com>
2012-04-11 17:49:06 -0500

    wlan: Upgrading firmware to R5.00.14 package
    Firmware for WL128X:
    wl128x-fw-4-sr.bin    FRev 7.3.7.0.105
    wl128x-fw-4-mr.bin    FRev 7.5.4.0.20
    wl128x-fw-4-plt.bin   FPLT 7.3.7.0.105
    
    Firmware for WL127X:
    wl127x-fw-4-sr.bin    FRev 6.3.7.0.105
    wl127x-fw-4-mr.bin    FRev 6.5.4.0.20
    wl127x-fw-4-plt.bin   FPLT 6.3.7.0.105
    
    R5.00.14 package upgradation needs the following patches also:
    for external/wpa_supplicant_8:
    	http://review.omapzoom.org/21329
    for hardware/ti/wlan:
    	http://review.omapzoom.org/21330
    	http://review.omapzoom.org/21331
    
    Change-Id: I1728d621a9acad0f27ae5e828e4118fff13d9c0e
    Signed-off-by: Panduranga Mallireddy <x0125717@ti.com>

--- wl12xx/wlan/wl12xx-wlan-fw-products.mk
+++ wl12xx/wlan/wl12xx-wlan-fw-products.mk
@@ -5,16 +5,5 @@ PRODUCT_COPY_FILES += \
         device/ti/proprietary-open/wl12xx/wlan/wl127x-fw-4-mr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin \
         device/ti/proprietary-open/wl12xx/wlan/wl127x-fw-4-plt.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin \
         device/ti/proprietary-open/wl12xx/wlan/wl127x-fw-4-sr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin \
-        device/ti/proprietary-open/wl12xx/wlan/wl128x-fw-mr.bin.r4:system/etc/firmware/ti-connectivity/wl128x-fw-mr.bin.r4 \
-        device/ti/proprietary-open/wl12xx/wlan/wl128x-fw-mr.bin.r5:system/etc/firmware/ti-connectivity/wl128x-fw-mr.bin.r5 \
-        device/ti/proprietary-open/wl12xx/wlan/wl128x-fw-mr_plt.bin.r4:system/etc/firmware/ti-connectivity/wl128x-fw-mr_plt.bin.r4 \
-        device/ti/proprietary-open/wl12xx/wlan/wl128x-fw-mr_plt.bin.r5:system/etc/firmware/ti-connectivity/wl128x-fw-mr_plt.bin.r5 \
-        device/ti/proprietary-open/wl12xx/wlan/wl128x-fw-r4sp2-multirole-roc.bin:system/etc/firmware/ti-connectivity/wl128x-fw-multirole-roc.bin \
-        device/ti/proprietary-open/wl12xx/wlan/wl128x-fw-r4sp2-multirole-plt.bin:system/etc/firmware/ti-connectivity/wl128x-fw-multirole-plt.bin \
         device/ti/proprietary-open/wl12xx/wlan/wl1271-nvs.bin:system/etc/firmware/ti-connectivity/wl1271-nvs.bin \
-        device/ti/proprietary-open/wl12xx/wlan/wl127x-fw-mr.bin.r4:system/etc/firmware/ti-connectivity/wl127x-fw-mr.bin.r4 \
-        device/ti/proprietary-open/wl12xx/wlan/wl127x-fw-mr.bin.r5:system/etc/firmware/ti-connectivity/wl127x-fw-mr.bin.r5 \
-        device/ti/proprietary-open/wl12xx/wlan/wl127x-fw-mr_plt.bin.r4:system/etc/firmware/ti-connectivity/wl127x-fw-mr_plt.bin.r4 \
-        device/ti/proprietary-open/wl12xx/wlan/wl1271-fw-r4sp2-multirole-roc.bin:system/etc/firmware/ti-connectivity/wl1271-fw-multirole-roc.bin \
-        device/ti/proprietary-open/wl12xx/wlan/wl1271-fw-r4sp2-multirole-plt.bin:system/etc/firmware/ti-connectivity/wl1271-fw-multirole-plt.bin \
         device/ti/proprietary-open/wl12xx/wlan/wl1271-nvs_127x.bin:system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin
