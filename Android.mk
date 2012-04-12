1f103671a59b7832d70e36270d00729d4e991d3f
James W. Mills <jameswmills@ti.com>
2012-04-12 14:06:05 -0500

    Updated Ducati installation logic
    This patch moves the ducati installation logic into the top-level
    Android.mk for proprietary-open, and generalizes the procedure so that
    new devices can be added easily.  Benefis include:
    
    * Unpacking into an intermediate location
    
    * Only unpacked if enabled for a specific device
    
    * All devices can use the same PRODUCT_PACKAGE definition
    
    * User-defined ducati TGZ location
    
    Change-Id: I48314920b138230e73b3a56e1ff899ad82289b59
    Signed-off-by: James W. Mills <jameswmills@ti.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,33 @@
 LOCAL_PATH := $(my-dir)
 $(hide $(shell $(LOCAL_PATH)/untar.sh $(LOCAL_PATH)/omap4 sgx $(LOCAL_PATH)/omap4/sgx.tgz))
 $(hide $(shell $(LOCAL_PATH)/untar.sh $(LOCAL_PATH)/wl12xx wpan $(LOCAL_PATH)/wl12xx/wpan.tgz))
+
+# Install Ducati from the TGZ defined in the device-inherited mk file
+#############################################
+ifneq ($(DUCATI_TGZ),)
+ti-ducati.untarred_intermediates := $(call intermediates-dir-for, FAKE, ti-ducati.untarred)
+ti-ducati.untarred_timestamp := $(ti-ducati.untarred_intermediates)/stamp
+
+$(ti-ducati.untarred_timestamp) : $(DUCATI_TGZ)
+	@echo "Unzip $(dir $@) <- $<)"
+	$(hide) rm -rf $(dir $@) && mkdir -p $(dir $@)
+	$(hide) tar -C $(dir $@) -zxf $<
+	$(hide) touch $@
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ducati-m3.bin
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/firmware
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE) : PRIVATE_SRC := $(ti-ducati.untarred_intermediates)/ducati-m3.bin
+$(LOCAL_BUILT_MODULE) : $(ti-ducati.untarred_timestamp) | $(ACP)
+	@echo "Copy $@ <- $(PRIVATE_SRC)"
+	@mkdir -p $(dir $@)
+	$(hide) $(ACP) -fp $(PRIVATE_SRC) $@
+endif
+#############################################
+
 include $(call all-subdir-makefiles)
