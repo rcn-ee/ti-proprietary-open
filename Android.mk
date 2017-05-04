f8a11b9658a5aacdeea02b0c54e20b5f3ef23ae3
Jason Reeder <jreeder@ti.com>
2017-05-04 16:58:31 -0500

    am57x: add general purpose PRU firmwares
    This commit adds the PRU firmwares necessary to enable the
    general purpose PRU rpmsg examples on the AM57xx boards.
    
    Change-Id: I19fcd2d2c36668a9c5c3dc43c7aaa0a65f4ddf1e
    Signed-off-by: Jason Reeder <jreeder@ti.com>

--- Android.mk
+++ Android.mk
@@ -142,3 +142,75 @@ $(LOCAL_BUILT_MODULE) : $(ti-prueth.untarred_timestamp) | $(ACP)
 	$(hide) $(ACP) -fp $(PRIVATE_SRC) $@
 endif
 #############################################
+
+#############################################
+# Install pru-icss firmware
+#############################################
+ifneq ($(PRUICSS_TGZ),)
+ti-pru-icss.untarred_intermediates := $(call intermediates-dir-for, FAKE, ti-pru-icss.untarred)
+ti-pru-icss.untarred_timestamp := $(ti-pru-icss.untarred_intermediates)/stamp
+
+$(ti-pru-icss.untarred_timestamp) : $(PRUICSS_TGZ)
+	@echo "Unzip $(dir $@) <- $<)"
+	$(hide) rm -rf $(dir $@) && mkdir -p $(dir $@)
+	$(hide) tar -C $(dir $@) -zxf $<
+	$(hide) touch $@
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := am57xx-pru1_0-fw
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/firmware
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE) : PRIVATE_SRC := $(ti-pru-icss.untarred_intermediates)/am57xx-pru1_0-fw
+$(LOCAL_BUILT_MODULE) : $(ti-pru-icss.untarred_timestamp) | $(ACP)
+	@echo "Copy $@ <- $(PRIVATE_SRC)"
+	@mkdir -p $(dir $@)
+	$(hide) $(ACP) -fp $(PRIVATE_SRC) $@
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := am57xx-pru1_1-fw
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/firmware
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE) : PRIVATE_SRC := $(ti-pru-icss.untarred_intermediates)/am57xx-pru1_1-fw
+$(LOCAL_BUILT_MODULE) : $(ti-pru-icss.untarred_timestamp) | $(ACP)
+	@echo "Copy $@ <- $(PRIVATE_SRC)"
+	@mkdir -p $(dir $@)
+	$(hide) $(ACP) -fp $(PRIVATE_SRC) $@
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := am57xx-pru2_0-fw
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/firmware
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE) : PRIVATE_SRC := $(ti-pru-icss.untarred_intermediates)/am57xx-pru2_0-fw
+$(LOCAL_BUILT_MODULE) : $(ti-pru-icss.untarred_timestamp) | $(ACP)
+	@echo "Copy $@ <- $(PRIVATE_SRC)"
+	@mkdir -p $(dir $@)
+	$(hide) $(ACP) -fp $(PRIVATE_SRC) $@
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := am57xx-pru2_1-fw
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/firmware
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE) : PRIVATE_SRC := $(ti-pru-icss.untarred_intermediates)/am57xx-pru2_1-fw
+$(LOCAL_BUILT_MODULE) : $(ti-pru-icss.untarred_timestamp) | $(ACP)
+	@echo "Copy $@ <- $(PRIVATE_SRC)"
+	@mkdir -p $(dir $@)
+	$(hide) $(ACP) -fp $(PRIVATE_SRC) $@
+
+endif
+#############################################
